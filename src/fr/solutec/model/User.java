bbd2001e15108a926d8e811cdbeb701c1eb01713
stagiaire <stagiaire@192.168.1.80>
2019-10-01 13:50:57 +0200

    Ajout class User

--- src/fr/solutec/model/User.java
+++ src/fr/solutec/model/User.java
@@ -5,6 +5,8 @@
  */
 package fr.solutec.model;
 
+import java.util.logging.Logger;
+
 /**
  *
  * @author Joel B
@@ -18,6 +20,76 @@ public class User {
     private int age;
     private int taille;
     private double poids;
+
+    public User() {
+    }
+
+    public User(String pseudo, String mail, String sexe, int age, int taille, double poids) {
+        this.pseudo = pseudo;
+        this.mail = mail;
+        this.sexe = sexe;
+        this.age = age;
+        this.taille = taille;
+        this.poids = poids;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public double getPoids() {
+        return poids;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    public void setPoids(double poids) {
+        this.poids = poids;
+    }
+   
+    
 
     
 }
