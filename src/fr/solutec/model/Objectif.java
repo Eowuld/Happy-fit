a0e2ec247fdc700cb256cf0d5810820fc62e1490
TLeMet <stagiaire@192.168.1.46>
2019-10-01 13:48:52 +0200

    classe Objectif

---
+++ src/fr/solutec/model/Objectif.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.solutec.model;
+
+/**
+ *
+ * @author TLeMet
+ */
+public class Objectif {
+    private double poids;
+    private double objectifPoids;
+    private double objectifDistance;
+    private double objectifDuree;
+
+    public Objectif() {
+    }
+
+    public Objectif(double poids, double objectifPoids, double objectifDistance, double objectifDuree) {
+        this.poids = poids;
+        this.objectifPoids = objectifPoids;
+        this.objectifDistance = objectifDistance;
+        this.objectifDuree = objectifDuree;
+    }
+
+    public double getPoids() {
+        return poids;
+    }
+
+    public double getObjectifPoids() {
+        return objectifPoids;
+    }
+
+    public double getObjectifDistance() {
+        return objectifDistance;
+    }
+
+    public double getObjectifDuree() {
+        return objectifDuree;
+    }
+
+    public void setPoids(double poids) {
+        this.poids = poids;
+    }
+
+    public void setObjectifPoids(double objectifPoids) {
+        this.objectifPoids = objectifPoids;
+    }
+
+    public void setObjectifDistance(double objectifDistance) {
+        this.objectifDistance = objectifDistance;
+    }
+
+    public void setObjectifDuree(double objectifDuree) {
+        this.objectifDuree = objectifDuree;
+    }
+        
+}
+
